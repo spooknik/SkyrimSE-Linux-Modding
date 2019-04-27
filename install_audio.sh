a313daf188c059a88778a79a4b159af248e91afe
spooknik <40317809+spooknik@users.noreply.github.com>
2019-04-27 07:46:01 +0200

    Update install_audio.sh

--- install_audio.sh
+++ install_audio.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Installing FAudio for Skyrim SE"
 cd faudio-19.02
-WINEPREFIX="$HOME/.steam/steam/steamapps/compatdata/489830" ./wine_setup_faudio.sh
+WINEPREFIX="$HOME/.steam/steam/steamapps/compatdata/489830/pfx" ./wine_setup_faudio.sh
